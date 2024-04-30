a3965250c810bdf5b0c4b5ec205c47000ffb5838
Nathaniel Miklas <nmiklas@vargomail.com>
2024-04-30 07:45:55 -0400

    not using this anymore

--- EasyLogger/EasyLogger.py
+++ EasyLogger/EasyLogger.py
@@ -72,103 +72,103 @@ def getEasyLogger(
     return logger
 
 
-class EasyLogger:
-    def __init__(
-        self,
-        logsDirectory: str,
-        ignorePrintToConsole: bool,
-        loggingLevel: str = 'INFO',
-        amountLogsToKeep: int | None = None,
-        loggerName: str = 'MiklasLogger',
-    ):
-        # self.loggingLevel = loggingLevel
+# class EasyLogger:
+#     def __init__(
+#         self,
+#         logsDirectory: str,
+#         ignorePrintToConsole: bool,
+#         loggingLevel: str = 'INFO',
+#         amountLogsToKeep: int | None = None,
+#         loggerName: str = 'MiklasLogger',
+#     ):
+#         # self.loggingLevel = loggingLevel
 
-        self._amountLogsToKeep = amountLogsToKeep
+#         self._amountLogsToKeep = amountLogsToKeep
 
-        if loggingLevel is None:
-            loggingLevel = 'INFO'
+#         if loggingLevel is None:
+#             loggingLevel = 'INFO'
 
-        if ignorePrintToConsole is None:
-            self.ignorePrintToConsole is False
-        self.ignorePrintToConsole = ignorePrintToConsole
+#         if ignorePrintToConsole is None:
+#             self.ignorePrintToConsole is False
+#         self.ignorePrintToConsole = ignorePrintToConsole
 
-        now = datetime.now()
-        nowString = now.strftime('%Y%m%d%H%M%S')
+#         now = datetime.now()
+#         nowString = now.strftime('%Y%m%d%H%M%S')
 
-        self._logsDirectory = logsDirectory
-        self._logFileName = nowString + '_' + self._logFileSuffix + '.log'
+#         self._logsDirectory = logsDirectory
+#         self._logFileName = nowString + '_' + self._logFileSuffix + '.log'
 
-        # self.logsDirectory = logsDirectory
+#         # self.logsDirectory = logsDirectory
 
-        if not os.path.exists(logsDirectory):
-            os.makedirs(logsDirectory)
+#         if not os.path.exists(logsDirectory):
+#             os.makedirs(logsDirectory)
 
-        logger = logging.getLogger(loggerName)
+#         logger = logging.getLogger(loggerName)
 
-        handler = logging.FileHandler(self._logsDirectory + self._logFileName, mode='w')
+#         handler = logging.FileHandler(self._logsDirectory + self._logFileName, mode='w')
 
-        formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(message)s')
+#         formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(message)s')
 
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(loggingLevel)
+#         handler.setFormatter(formatter)
+#         logger.addHandler(handler)
+#         logger.setLevel(loggingLevel)
 
-        self._logger = logger
+#         self._logger = logger
 
-        if self._amountLogsToKeep:
-            logClean(
-                self._logger,
-                self._logsDirectory,
-                self._logFileSuffix,
-                self._amountLogsToKeep,
-            )
+#         if self._amountLogsToKeep:
+#             logClean(
+#                 self._logger,
+#                 self._logsDirectory,
+#                 self._logFileSuffix,
+#                 self._amountLogsToKeep,
+#             )
 
-    def getCurrentLevel(self):
-        return self._logger.level
+#     def getCurrentLevel(self):
+#         return self._logger.level
 
-    def _printMessage(self, message: str) -> bool:
-        if self.ignorePrintToConsole:
-            return False
-        print(message)
-        return True
+#     def _printMessage(self, message: str) -> bool:
+#         if self.ignorePrintToConsole:
+#             return False
+#         print(message)
+#         return True
 
-    def writeInfo(self, message: str):
-        self._logger.info(message)
-        self._printMessage(message)
+#     def writeInfo(self, message: str):
+#         self._logger.info(message)
+#         self._printMessage(message)
 
-    def writeDebug(self, message: str):
-        self._logger.debug(message)
-        self._printMessage(message)
+#     def writeDebug(self, message: str):
+#         self._logger.debug(message)
+#         self._printMessage(message)
 
-    def writeCrticial(self, message: str):
-        self._logger.critical(message)
-        self._printMessage(message)
+#     def writeCrticial(self, message: str):
+#         self._logger.critical(message)
+#         self._printMessage(message)
 
 
-def logClean(logger, logsDirectory: str, loggerName: str, amountLogsToKeep: int):
-    filesInDir = os.listdir(logsDirectory)
-    ticketLogFiles = [file for file in filesInDir if file.startswith(loggerName)]
+# def logClean(logger, logsDirectory: str, loggerName: str, amountLogsToKeep: int):
+#     filesInDir = os.listdir(logsDirectory)
+#     ticketLogFiles = [file for file in filesInDir if file.startswith(loggerName)]
 
-    if len(ticketLogFiles) >= amountLogsToKeep:
-        fileswithDates = [
-            {
-                'date': os.path.getctime(os.path.join(logsDirectory, file)),
-                'fileName': file,
-            }
-            for file in ticketLogFiles
-        ]
+#     if len(ticketLogFiles) >= amountLogsToKeep:
+#         fileswithDates = [
+#             {
+#                 'date': os.path.getctime(os.path.join(logsDirectory, file)),
+#                 'fileName': file,
+#             }
+#             for file in ticketLogFiles
+#         ]
 
-        fileswithDatesSorted = sorted(fileswithDates, key=lambda file: file['date'])
-        [
-            (
-                logger.debug(
-                    f"will delete file: {os.path.join(logsDirectory, file['fileName'])}"
-                ),
-                os.remove(os.path.join(logsDirectory, file['fileName'])),
-            )
-            for file in fileswithDatesSorted[
-                0 : len(fileswithDatesSorted) - amountLogsToKeep
-            ]
-        ]
-    else:
-        logger.debug("Cant't delete more logs than we have.")
+#         fileswithDatesSorted = sorted(fileswithDates, key=lambda file: file['date'])
+#         [
+#             (
+#                 logger.debug(
+#                     f"will delete file: {os.path.join(logsDirectory, file['fileName'])}"
+#                 ),
+#                 os.remove(os.path.join(logsDirectory, file['fileName'])),
+#             )
+#             for file in fileswithDatesSorted[
+#                 0 : len(fileswithDatesSorted) - amountLogsToKeep
+#             ]
+#         ]
+#     else:
+#         logger.debug("Cant't delete more logs than we have.")
